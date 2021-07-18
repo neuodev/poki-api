398c94c79d94b23ad0c1c5304bd32618e9843154
Ahmed Ibrahim <ib153507@gmail.com>
2021-07-18 22:55:44 +0200

    Add pagination

--- src/components/Pokemons/Pokemons.js
+++ src/components/Pokemons/Pokemons.js
@@ -2,16 +2,20 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import PokemonCard from "./PokemonCard";
 
-// add pagination , items perpage, and card
 const Pokemons = () => {
   const [pokemons, setPokemons] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(
+    "https://pokeapi.co/api/v2/pokemon/?offset=10&limit=10"
+  );
+  const [prevPage, setPrevPage] = useState(null);
+  const [nextPage, setNextPage] = useState(null);
   useEffect(() => {
     try {
       async function fetch() {
         setLoading(true);
-        const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon/");
+        const { data } = await axios.get(currentPage);
         // populate the data
         /*
               The data return from the api only include the name and the url to get the data for each pokemon 
@@ -26,6 +30,7 @@ const Pokemons = () => {
 
         setLoading(false);
         setPokemons(allPokemons);
+        setNextPage(data.next);
       }
 
       fetch();
@@ -38,13 +43,49 @@ const Pokemons = () => {
       setLoading(false);
       setPokemons([]);
     };
-  }, []);
+  }, [currentPage]);
+
+  const getNextPage = () => {
+    setPrevPage(currentPage);
+    setCurrentPage(nextPage);
+  };
+
+  const getPrevPage = () => {
+    setCurrentPage(prevPage);
+    setPrevPage(null);
+  };
+
   return (
     <div>
-      <div className="mx-4 grid  grid-cols-12 gap-4 mt-4">
-        {pokemons.map((pokemon) => (
-          <PokemonCard key={pokemon.id} pokemon={pokemon} />
-        ))}
+      {loading ? (
+        <h1 className="text-center mt-4">Loading</h1>
+      ) : error ? (
+        <h1 className="text-red-500 mt-4 text-center">{error}</h1>
+      ) : (
+        <div className="mx-4 grid  grid-cols-12 gap-4 mt-4">
+          {pokemons.map((pokemon) => (
+            <PokemonCard key={pokemon.id} pokemon={pokemon} />
+          ))}
+        </div>
+      )}
+
+      <div className="w-full my-10 flex items-center justify-center">
+        <button
+          className={`px-3 py-2 mr-2 rounded-md shadow-md ${
+            prevPage ? "bg-blue-200 text-blue-500" : "text-gray-300 bg-gray-200"
+          }`}
+          onClick={getPrevPage}
+        >
+          Prev
+        </button>
+        <button
+          className={`px-3 py-2 mr-2 rounded-md shadow-md ${
+            nextPage ? "bg-blue-200 text-blue-500" : "text-gray-300 bg-gray-200"
+          }`}
+          onClick={getNextPage}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
